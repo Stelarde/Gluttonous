c4cfaf54c4221492321739b815485fd08acf14ff
Stelarde <maksimyudin31@gmail.com>
2022-11-30 15:56:06 +0300

    Init commit

---
+++ example1/.section.php
@@ -0,0 +1,7 @@
+<?
+$sSectionName = "example1";
+$arDirProperties = Array(
+   "description" => "Это example 1",
+   "keywords" => "Example, Один"
+);
+?>
---
+++ example2/.section.php
@@ -0,0 +1,8 @@
+<?
+$sSectionName="example2";
+$arDirProperties = Array(
+    "description" => "Это example 2",
+    "keywords" => "Example, Два",
+    "some_property" => "some_value"
+ );
+?>
